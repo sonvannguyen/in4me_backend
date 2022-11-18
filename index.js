8c82b6263bf60704fb19d10f84de57e781e78ac0
sonvannguyen <88154833+sonvannguyen@users.noreply.github.com>
2022-11-18 09:37:55 +0700

    Update index.js

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ const server = app.listen(process.env.PORT || 5000, () => {
 
 const io = socket(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "https://in4me.netlify.app",
     },
 });
 
